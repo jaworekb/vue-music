0a931ce34f381151e73a021b3c7c71dff3cdfb4e
Bartosz Jaworek <jaworekb@gmail.com>
2018-03-17 14:48:56 +0100

    main components

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,28 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="app">
+        <menu-comp></menu-comp>
+        <router-view/>
+    </div>
 </template>
 
 <script>
 import Menu from './components/Menu.vue'
+
 export default {
   name: 'App',
   components: {
-    Menu
+    MenuComp: Menu
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
