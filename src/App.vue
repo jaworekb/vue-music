bd9af06e51d0515334a647adf5d87c6067f9057b
Bartosz Jaworek <jaworekb@gmail.com>
2018-03-17 15:03:02 +0100

    scss

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="app">
+        <p class="test">aaaaa</p>
         <menu-comp></menu-comp>
         <router-view/>
     </div>
@@ -16,8 +17,11 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
     #app {
+        .test {
+            color: red;
+        }
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
