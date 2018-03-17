b7b2efd608a0f01212d4883e9a3b0bc0d42bd21a
thesebas <thesebas@thesebas.net>
2018-03-17 18:02:17 +0100

    Artist details

--- src/components/ArtistDetails.vue
+++ src/components/ArtistDetails.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row artist-details-header">
     <div class="col-xs-12 col-sm-4">
-      <img v-bind:src="image" class="img-thumbnail"/>
+      <img :src="image" class="img-thumbnail"/>
     </div>
     <ul class="col">
       <li><h2>{{name}}</h2></li>
@@ -16,13 +16,13 @@ export default {
   props: ['artist'],
   computed: {
     image () {
-      return 'http://userserve-ak.last.fm/serve/85/5112299.jpg'
+      return this.artist.image ? this.artist.image.filter(({size}) => size === 'large')[0]['#text'] : ''
     },
     name () {
-      return 'cher'
+      return this.artist.name
     },
     bio () {
-      return 'some long text'
+      return this.artist.bio ? this.artist.bio.summary : ''
     }
   }
 }
